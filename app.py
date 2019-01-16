a8353f5fd40a423e19bf242b2680abb94ca31755
wg18618262234 <https://github.com/wg18618262234/JDReptail.git>
2019-01-16 11:27:02 +0800

    1 return json 2 add monster info

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome Guaji</h1>'
+
+
+@app.errorhandler(404)
+def errorhandler(error):
+    return render_template('page_not_found.html'),404
+
+# 玩家信息添加
+@app.route('/player/info/add/', methods=['GET', 'POST'])
+def player_info_get():
+    if request.method == 'POST':
+        from models import Player_info
+        Pname = str(request.form.get('Pname'))
+        Php = str(request.form.get('Php'))
+        Ppower = str(request.form.get('Ppower'))
+        db.session.add(Player_info(Pname, Php, Ppower))
+        return render_template('player_info_add.html')
+    else:
+        return render_template('player_info_add.html')
+
+
+# 玩家信息根据Pid查询
+@app.route('/player/info/show/', methods=['GET', 'POST'])
+def player_info_show():
+    from models import Player_info
+    if request.method == 'POST':
+        Pid = str(request.form.get('Pid'))
+        player_datas = db.session.query(Player_info).filter_by(id=Pid).all()
+        result = []
+        for player_data in player_datas:
+            result.append(player_data.to_json())
+        return jsonify(result)
+    else:
+        Pid = str(request.args.get('Pid'))
+        player_datas = db.session.query(Player_info).filter_by(id=Pid).all()
+        result = []
+        for player_data in player_datas:
+            result.append(player_data.to_json())
+        return jsonify(result)
+
+
+# 怪物信息添加
+@app.route('/monster/info/add/', methods=['GET', 'POST'])
+def monster_info_get():
+    if request.method == 'POST':
+        from models import Monster_info
+        Pname = str(request.form.get('Pname'))
+        Php = str(request.form.get('Php'))
+        Ppower = str(request.form.get('Ppower'))
+        db.session.add(Monster_info(Pname, Php, Ppower))
+        return render_template('monster_info_add.html')
+    else:
+        return render_template('monster_info_add.html')
+
+
+# 怪物信息根据Pid查询
+@app.route('/monster/info/show/', methods=['GET', 'POST'])
+def monster_info_show():
+    from models import Monster_info
+    if request.method == 'POST':
+        Pid = str(request.form.get('Pid'))
+        player_datas = db.session.query(Monster_info).filter_by(id=Pid).all()
+        result = []
+        for player_data in player_datas:
+            result.append(player_data.to_json())
+        return jsonify(result)
+    else:
+        Pid = str(request.args.get('Pid'))
+        player_datas = db.session.query(Monster_info).filter_by(id=Pid).all()
+        result = []
+        for player_data in player_datas:
+            result.append(player_data.to_json())
+        return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-from player import db
-import json
+from app import db
 
 
-# 创建模型对象
+# 创建玩家信息模型对象
 class Player_info(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20))
@@ -15,4 +14,32 @@ class Player_info(db.Model):
         self.power = power
 
     def __repr__(self):
-        return json.dumps({'name':self.name,'hp':self.hp,'power':self.power})
+        return {'name': self.name, 'hp': self.hp, 'power': self.power}
+
+    def to_json(self):
+        dict = self.__dict__
+        if "_sa_instance_state" in dict:
+            del dict["_sa_instance_state"]
+        return dict
+
+
+# 创建小怪信息模型对象
+class Monster_info(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20))
+    hp = db.Column(db.String(100))
+    power = db.Column(db.String(100))
+
+    def __init__(self, name, hp, power):
+        self.name = name
+        self.hp = hp
+        self.power = power
+
+    def __repr__(self):
+        return {'name': self.name, 'hp': self.hp, 'power': self.power}
+
+    def to_json(self):
+        dict = self.__dict__
+        if "_sa_instance_state" in dict:
+            del dict["_sa_instance_state"]
+        return dict
